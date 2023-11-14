a3ec8fd5a2ba4d8be35d44ec1d8fde6313e8236e
Mina Markham <mina.markham@gmail.com>
2023-11-13 18:19:19 -0800

    fix build. again.

--- next.config.js
+++ next.config.js
@@ -1,3 +1,15 @@
-module.exports = {
+const nextConfig = {
 	images: { unoptimized: true },
+	output: 'export',
+
+	// Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
+	// trailingSlash: true,
+
+	// Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
+	// skipTrailingSlashRedirect: true,
+
+	// Optional: Change the output directory `out` -> `dist`
+	distDir: 'www',
 };
+
+module.exports = nextConfig;
